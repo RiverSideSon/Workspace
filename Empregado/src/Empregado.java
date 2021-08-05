117a64c42ea529a2cf187286ba91927a12890ae3
RiverSideSon <chesmt@hotmail.com>
2021-08-05 18:07:06 -0300

    Alteração 1

--- Empregado/src/Empregado.java
+++ Empregado/src/Empregado.java
@@ -1,4 +1,17 @@
-
 public class Empregado {
+	String nome;
+	String cargo;
+	double salario;
+	
+	void imprimir() {
+		System.out.println("Nome: "+nome);
+		System.out.println("Cargo: "+cargo);
+		System.out.println("Salario: "+salario);
+}
+	
+	double aumentarSalario (double percentual) {
+		salario = salario + salario * percentual / 100;
+		return salario;
+	}
 
 }
--- Empregado/src/Principal.java
+++ Empregado/src/Principal.java
@@ -1,4 +1,21 @@
-
 public class Principal {
+	public static void main(String[] args) {
+		Empregado e1;
+		
+		e1 = new Empregado();
 
+		e1.nome = "Isidro";
+		e1.cargo = "Professor";
+		e1.salario = 10000;
+		
+		System.out.println("-- Antes do aumento");
+		
+		e1.imprimir();
+
+		e1.aumentarSalario(10);
+		
+		System.out.println("-- Depois do aumento");
+		
+		e1.imprimir();
+	}
 }
